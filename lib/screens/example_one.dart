85fb6141eda06fec74decc81200969fd60a5c0a2
Yaseen Wakeel <yaseen.wakeel@sumatosolutions.com>
2022-08-16 03:16:10 +0500

    example one : widget rebuild problem solved

--- lib/screens/example_one.dart
+++ lib/screens/example_one.dart
@@ -14,7 +14,7 @@ class _ExampleOneState extends State<ExampleOne> {
   @override
   Widget build(BuildContext context) {
     final sliderProvider =
-        Provider.of<ExampleOneProvider>(context, listen: true);
+        Provider.of<ExampleOneProvider>(context, listen: false);
     print('build');
     return Scaffold(
       appBar: AppBar(
@@ -25,39 +25,55 @@ class _ExampleOneState extends State<ExampleOne> {
       body: Column(
         children: [
           Text('slider'),
-          Slider(
-            value: sliderProvider.val,
-            onChanged: (value) {
-              sliderProvider.SetValue(value); // provider method for seting val
-              print(sliderProvider.val);
+          Consumer<ExampleOneProvider>(
+            builder: (context, provider, child) {
+              print('slider widget build');
+              return Slider(
+                value: provider.val,
+                onChanged: (value) {
+                  sliderProvider.SetValue(
+                      value); // provider method for seting val
+                  print(sliderProvider.val);
+                },
+                min: 0.1,
+                max: 1.0,
+              );
             },
-            min: 0.1,
-            max: 1.0,
           ),
           Row(
             children: [
               Expanded(
-                child: Container(
-                  height: 100,
-                  color: Colors.green.withOpacity(sliderProvider.val), // getter
-                  child: Center(
-                    child: Text(
-                      'Container 1',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
+                child: Consumer<ExampleOneProvider>(
+                  builder: (context, provider, child) {
+                    print('container1 widget build');
+                    return Container(
+                      height: 100,
+                      color: Colors.green.withOpacity(provider.val), // getter
+                      child: Center(
+                        child: Text(
+                          'Container 1',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ),
               Expanded(
-                child: Container(
-                  height: 100,
-                  color: Colors.red.withOpacity(sliderProvider.val), // getter
-                  child: Center(
-                    child: Text(
-                      'Container 2',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
+                child: Consumer<ExampleOneProvider>(
+                  builder: (context, provider, child) {
+                    print('container2 widget build');
+                    return Container(
+                      height: 100,
+                      color: Colors.red.withOpacity(provider.val), // getter
+                      child: Center(
+                        child: Text(
+                          'Container 2',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    );
+                  },
                 ),
               )
             ],
